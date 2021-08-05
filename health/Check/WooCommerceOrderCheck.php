faa5ea3e8208fc1c6e1b8e3022d9a259094c812d
Nils Langner <nils.langner@leankoala.com>
2021-08-05 14:26:32 +0200

    Order check as class

---
+++ health/Check/WooCommerceOrderCheck.php
@@ -0,0 +1,104 @@
+<?php
+
+use Leankoala\HealthFoundation\Check\Check;
+use Leankoala\HealthFoundation\Check\MetricAwareResult;
+use Leankoala\HealthFoundation\Check\Result;
+
+/**
+ * Class WordPressOrderCheck
+ *
+ * This check checks if there where enough orders within the last hour in the installed WooCommerce
+ * shop.
+ *
+ * @author Nils Langner <nils.langner@leankoala.com>
+ * created 2021-08-05
+ */
+class WooCommerceOrderCheck implements Check
+{
+    /**
+     * @inheritDoc
+     */
+    public function run()
+    {
+        $isPeakTime = $this->isPeakTime();
+        $orderCount = $this->getLastHourOrderCount();
+
+        if ($isPeakTime) {
+            $limit = get_option(Koality::CONFIG_WOOCOMMERCE_ORDER_PEAK_KEY);
+        } else {
+            $limit = get_option(Koality::CONFIG_WOOCOMMERCE_ORDER_PEAK_OFF_KEY);
+        }
+
+        if ($limit > $orderCount) {
+            $result = new MetricAwareResult(
+                Result::STATUS_FAIL,
+                'Not enough orders within the last hour.'
+            );
+        } else {
+            $result = new MetricAwareResult(
+                Result::STATUS_PASS,
+                'Enough orders within the last hour.'
+            );
+        }
+
+        $result->setLimit($limit);
+        $result->setMetric($orderCount, 'orders');
+
+        return $result;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getIdentifier()
+    {
+        return 'WordPressOrderCheck';
+    }
+
+    /**
+     * Return the number of orders that were done within the last hour.
+     *
+     * @return int
+     */
+    private function getLastHourOrderCount()
+    {
+        $query = new WC_Order_Query(array(
+            'date_created' => '>' . (time() - HOUR_IN_SECONDS),
+            'return' => 'ids',
+        ));
+
+        $orders = $query->get_orders();
+
+        return count($orders);
+    }
+
+    /**
+     * Return true if the current time is within the defined peak time.
+     *
+     * @return bool
+     */
+    private function isPeakTime()
+    {
+        $peakStart = get_option(Koality::CONFIG_WOOCOMMERCE_RUSH_HOUR_START_KEY);
+        $peakEnd = get_option(Koality::CONFIG_WOOCOMMERCE_RUSH_HOUR_END_KEY);
+        $includeWeekend = false;
+
+        $currentWeekDay = date('w');
+        $isWeekend = ($currentWeekDay == 0 || $currentWeekDay == 6);
+
+        $beginHour = $peakStart * 100;
+        $endHour = $peakEnd * 100;
+
+        $currentTime = (int)date('Hi');
+
+        if (!$includeWeekend && $isWeekend) {
+            return false;
+        }
+
+        if ($currentTime < $endHour && $currentTime > $beginHour) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
--- health/business.php
+++ health/business.php
@@ -1,90 +1,21 @@
 <?php
 
+use Leankoala\HealthFoundation\Check\Result;
+use Leankoala\HealthFoundation\HealthFoundation;
+use Leankoala\HealthFoundation\Result\Format\Koality\KoalityFormat;
+
 include 'init.php';
 
-/**
- * Get the number of orders in the last hour.
- *
- * @return int
- */
-function koality_getLastHourOrderCount()
-{
-    $query = new WC_Order_Query(array(
-        'date_created' => '>' . (time() - HOUR_IN_SECONDS),
-        'return' => 'ids',
-    ));
+include_once __DIR__ . '/Check/WooCommerceOrderCheck.php';
 
-    $orders = $query->get_orders();
-
-    return count($orders);
-}
-
-/**
- * Return if the current time is within the peak time slot.
- *
- * @param int $peakStart
- * @param int $peakEnd
- * @param bool $includeWeekend
- *
- * @return bool
- */
-function koality_isPeakTime()
-{
-    $peakStart = get_option(Koality::CONFIG_WOOCOMMERCE_RUSH_HOUR_START_KEY);
-    $peakEnd = get_option(Koality::CONFIG_WOOCOMMERCE_RUSH_HOUR_END_KEY);
-    $includeWeekend = false;
-
-    $currentWeekDay = date('w');
-    $isWeekend = ($currentWeekDay == 0 || $currentWeekDay == 6);
-
-    $allowRushHour = !($isWeekend && !$includeWeekend);
-
-    $beginHour = $peakStart * 100;
-    $endHour = $peakEnd * 100;
-
-    $currentTime = (int)date('Hi');
-
-    if (!$includeWeekend && $isWeekend) {
-        return false;
-    }
-
-    if ($currentTime < $endHour && $currentTime > $beginHour) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-$isPeakTime = koality_isPeakTime();
-$orderCount = koality_getLastHourOrderCount();
-
-if ($isPeakTime) {
-    $limit = get_option(Koality::CONFIG_WOOCOMMERCE_ORDER_PEAK_KEY);
-} else {
-    $limit = get_option(Koality::CONFIG_WOOCOMMERCE_ORDER_PEAK_OFF_KEY);
-}
-
-if ($limit > $orderCount) {
-    $orderCheck = new \Leankoala\HealthFoundation\Check\Basic\StaticCheck(
-        \Leankoala\HealthFoundation\Check\Result::KOALITY_IDENTIFIER_ORDERS_TOO_FEW,
-        \Leankoala\HealthFoundation\Check\Result::STATUS_FAIL,
-        'Not enough orders within the last hour.'
-    );
-} else {
-    $orderCheck = new \Leankoala\HealthFoundation\Check\Basic\StaticCheck(
-        \Leankoala\HealthFoundation\Check\Result::KOALITY_IDENTIFIER_ORDERS_TOO_FEW,
-        \Leankoala\HealthFoundation\Check\Result::STATUS_PASS,
-        'Enough orders within the last hour.'
-    );
-}
-
-$foundation->registerCheck($orderCheck);
+/** @var HealthFoundation $foundation */
+$foundation->registerCheck(new WooCommerceOrderCheck(), Result::KOALITY_IDENTIFIER_ORDERS_TOO_FEW);
 
 $runResult = $foundation->runHealthCheck();
 
-$formatter = new \Leankoala\HealthFoundation\Result\Format\Koality\KoalityFormat(
-    'Storage server is up and running.',
-    'Some problems occurred on storage server.'
+$formatter = new KoalityFormat(
+    'WooCommerce business metrics look good.',
+    'WooCommerce business metrics indicate problems.'
 );
 
 $formatter->handle(

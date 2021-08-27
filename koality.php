42124450f1b4f496e2f2a868f6dc5aa8651bcbdb
Nils Langner <nils.langner@leankoala.com>
2021-08-27 05:46:52 +0200

    description urls fix

--- koality.php
+++ koality.php
@@ -14,7 +14,7 @@
  *
  * @wordpress-plugin
  * Plugin Name:       koality.io
- * Plugin URI:        https://www.koality.io
+ * Plugin URI:        https://github.com/koality-io/WordPressPlugin
  * Description:       This plugin is used to connect WordPress and WooCommerce with koality.io to then perform important monitoring.
  * Version:           1.0.0
  * Author:            Nils Langner
